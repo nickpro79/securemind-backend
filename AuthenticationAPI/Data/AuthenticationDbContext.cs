4f630cf868a7a52e2e78a2b5ae8cbcfc1d542f7e
Nikhil Praveen <nikhilpraveen7777@gmail.com>
2024-09-03 14:55:42 +0530

    Added the AuthenitcationDbContext for creating the database and the nesscessary tables

---
+++ AuthenticationAPI/Data/AuthenticationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using AuthenticationAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationAPI.Data
+{
+    public class AuthenticationDbContext:DbContext
+    {
+        public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<User>().HasKey(u => u.UserId);
+            modelBuilder.Entity<Role>().HasKey(r => r.RoleId);
+            modelBuilder.Entity<UserRole>().HasKey(ur => new { ur.UserId, ur.RoleId });
+
+
+
+            modelBuilder.Entity<UserRole>()
+                .HasOne(ur => ur.User)
+                .WithMany(u => u.UserRoles)
+                .HasForeignKey(ur => ur.UserId);
+
+            modelBuilder.Entity<UserRole>()
+                .HasOne(ur => ur.Role)
+                .WithMany(r => r.UserRoles)
+                .HasForeignKey(ur => ur.RoleId);
+        }
+    }
+}
