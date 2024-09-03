60b642f93fd351c83ec5585d6059bfd9e3ee6001
Nikhil Praveen <nikhilpraveen7777@gmail.com>
2024-09-03 16:23:50 +0530

    added JWT Authentication to the services in Program.cs

--- AuthenticationAPI/Program.cs
+++ AuthenticationAPI/Program.cs
@@ -1,10 +1,13 @@
 using AuthenticationAPI.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+ConfigurationManager configuration = builder.Configuration;
 builder.Services.AddDbContext<AuthenticationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("UserDB"));
@@ -13,6 +16,25 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.SaveToken = true;
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidAudience = configuration["JWTAuthentication:ValidAudience"],
+            ValidIssuer = configuration["JWTAuthentication:ValidIssuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWTAuthentication:Key"]))
+        };
+    });
 
 var app = builder.Build();
 
