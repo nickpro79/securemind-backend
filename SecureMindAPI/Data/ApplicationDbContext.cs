dcdd2011436a1258c81e9ef1b07f47c74a88ffbc
Nikhil Praveen <nikhilpraveen7777@gmail.com>
2024-09-06 09:57:47 +0530

    Added DbContext and added the DbContext to services

---
+++ SecureMindAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SecureMindAPI.Models;
+
+namespace SecureMindAPI.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) 
+        {
+            
+        }
+        public DbSet<Incidents> CrimeIncidents { get; set; }
+        public DbSet<Reports> AnonymousReports { get; set; }
+        public DbSet<Counsellors> MentalHealthProfessionals { get; set; }
+        public DbSet<Location> Locations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Incidents>()
+            .HasOne(ci => ci.Location)
+            .WithMany(l => l.CrimeIncidents)
+            .HasForeignKey(ci => ci.LocationId);
+
+            modelBuilder.Entity<Reports>()
+            .HasOne(ar => ar.Location)
+            .WithMany(l => l.AnonymousReports)
+            .HasForeignKey(ar => ar.LocationId);
+
+            modelBuilder.Entity<Counsellors>()
+            .HasOne(mhp => mhp.Location)
+            .WithMany(l => l.MentalHealthProfessionals)
+            .HasForeignKey(mhp => mhp.LocationId);
+
+
+        }
+    }
+}
--- SecureMindAPI/Program.cs
+++ SecureMindAPI/Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using SecureMindAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SecureMind"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
