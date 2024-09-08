3394801416d3d65e69820ccd9d1cdb2091ac4201
Nikhil Praveen <nikhilpraveen7777@gmail.com>
2024-09-08 16:24:04 +0530

    Created Model Register DTO

---
+++ SecureMindAPI/DTOs/RegisterDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace SecureMindAPI.DTOs
+{
+    public class RegisterDTO
+    {
+        public int LocationId { get; set; }
+        public string Description { get; set; }
+        public DateTime ReportTime { get; set; }
+        public string Type { get; set; }
+    }
+}
