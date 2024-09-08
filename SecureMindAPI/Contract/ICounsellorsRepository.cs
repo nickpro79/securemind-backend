e652dfad2cd2b5f1851da8c968093cfb67db6f75
Nikhil Praveen <nikhilpraveen7777@gmail.com>
2024-09-08 11:17:21 +0530

    added GetAll and FilterBySpecialization to ICounsellorRepository

--- SecureMindAPI/Contract/ICounsellorsRepository.cs
+++ SecureMindAPI/Contract/ICounsellorsRepository.cs
@@ -1,7 +1,11 @@
-﻿namespace SecureMindAPI.Contract
+﻿using SecureMindAPI.DTOs;
+
+namespace SecureMindAPI.Contract
 {
     public interface ICounsellorsRepository
     {
-        public Task<>
+        public Task<IEnumerable<CounsellerDTO>> GetAll();
+        public Task<IEnumerable<CounsellerDTO>> FilterBySpecialization(string specialization);
+
     }
 }
