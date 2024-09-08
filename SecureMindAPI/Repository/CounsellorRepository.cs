5be2bbd4fd235ebdba5b9a46cc2a5630b1b8401a
Nikhil Praveen <nikhilpraveen7777@gmail.com>
2024-09-08 11:25:02 +0530

    Implemented GetAll method in CounsellorRepository

---
+++ SecureMindAPI/Repository/CounsellorRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SecureMindAPI.Contract;
+using SecureMindAPI.Data;
+using SecureMindAPI.DTOs;
+
+namespace SecureMindAPI.Repository
+{
+    public class CounsellorRepository : ICounsellorsRepository
+    {
+        private ApplicationDbContext _context;
+        public CounsellorRepository(ApplicationDbContext context)
+        {
+           _context = context;
+        }
+        public Task<IEnumerable<CounsellerDTO>> FilterBySpecialization(string specialization)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<CounsellerDTO>> GetAll()
+        {
+            var Counsellors = await _context.MentalHealthProfessionals.ToListAsync();
+            var result = new List<CounsellerDTO>();
+            Counsellors.ForEach(c =>
+            {
+                CounsellerDTO temp = new CounsellerDTO()
+                {
+                    Name = c.Name,
+                    Specialization = c.Specialization,
+                    ContactInfo = c.ContactInfo,
+                };
+                result.Add(temp);
+            });
+
+            return result;
+        }
+    }
+}
