169f7827cd437c6b03fd13ccafba3bd8f54a8ee8
Nikhil Praveen <nikhilpraveen7777@gmail.com>
2024-09-08 16:36:29 +0530

    Fixed bugs

---
+++ SecureMindAPI/DTOs/CounsellorDTO/CounsellerResponseDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace SecureMindAPI.DTOs.CounsellorRequestDTO
+{
+    public class CounsellerResponseDTO
+    {
+        public string Name { get; set; }
+        public string Specialization { get; set; }
+        public string ContactInfo { get; set; }
+    }
+}
