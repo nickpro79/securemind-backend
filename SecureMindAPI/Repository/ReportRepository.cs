b0b2eb3ec4f9dc5c7909059ca08196c6ede11347
Nikhil Praveen <nikhilpraveen7777@gmail.com>
2024-09-08 16:47:04 +0530

    added GetAllReport functionality

---
+++ SecureMindAPI/Repository/ReportRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SecureMindAPI.Contract;
+using SecureMindAPI.Data;
+using SecureMindAPI.DTOs;
+
+namespace SecureMindAPI.Repository
+{
+    public class ReportRepository : IReportRepository
+    {
+
+        private ApplicationDbContext _context;
+
+        public ReportRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public Task<bool> AddRepository(ReportDTO report)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<ReportDTO>> GetAllReports()
+        {
+            return await _context.CrimeIncidents
+            .Include(i => i.Location) // Include location data
+            .Select(i => new ReportDTO
+            {
+            Description = i.Description,
+            ReportTime = i.ReportTime,
+            Location = i.Location == null ? null : new LocationDto
+            {
+            Latitude = i.Location.Latitude,
+            Longitude = i.Location.Longitude
+            }
+            })
+            .ToListAsync();
+        }
+    }
+}
