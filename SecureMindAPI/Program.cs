27a57ecc1700d7e428e748152e4aaf89fd7b086e
Nikhil Praveen <nikhilpraveen7777@gmail.com>
2024-09-13 09:28:33 +0530

    Added JWT Authenitcation to SecureMindAPI

--- SecureMindAPI/Program.cs
+++ SecureMindAPI/Program.cs
@@ -1,12 +1,15 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using SecureMindAPI.Contract;
 using SecureMindAPI.Data;
 using SecureMindAPI.Repository;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+ConfigurationManager configuration = builder.Configuration;
 builder.Services.AddControllers();
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
@@ -32,6 +35,25 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("SecureMind"));
 });
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.SaveToken = true;
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidAudience = configuration["JWTAuthentication:ValidAudience"],
+            ValidIssuer = configuration["JWTAuthentication:ValidIssuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWTAuthentication:Key"]))
+        };
+    });
 var app = builder.Build();
 app.UseCors("AllowMyOrigin");
 
